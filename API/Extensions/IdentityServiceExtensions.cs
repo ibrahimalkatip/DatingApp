8c99afaccab60676f138e72aac01a68ec97f659b
TAGO\ialkhateeb <ibrahimalkatip@hotmail.com>
2024-02-06 12:06:18 +0300

    database changes

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -9,15 +9,15 @@ namespace API.Extensions
         public static IServiceCollection AddIdentityService(this IServiceCollection services, IConfiguration config)
         {
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"])),
-        ValidateIssuer = false,
-        ValidateAudience = false
-    };
-});
+              {
+                  options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                  {
+                      ValidateIssuerSigningKey = true,
+                      IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"])),
+                      ValidateIssuer = false,
+                      ValidateAudience = false
+                  };
+              });
             return services;
         }
     }
